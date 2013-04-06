dafd66cc8657277e7c37f55079cbd1ec1d3dbe86
Bo Zhi <bobo900128@gmail.com>
2013-04-07 05:43:03 +0800

    add additional options

--- genkernel.sh
+++ genkernel.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
 # time genkernel --menuconfig --no-clean --kerneldir=/usr/src/linux-tcel all
+# all
 time genkernel --kernel-config=/usr/src/linux-tcel/.config --no-clean --kerneldir=/usr/src/linux-tcel all
+# only kernel
+# time genkernel --kernel-config=/usr/src/linux-tcel/.config --no-clean --kerneldir=/usr/src/linux-tcel bzImage
+# kernel and modules
+# time genkernel --kernel-config=/usr/src/linux-tcel/.config --no-clean --kerneldir=/usr/src/linux-tcel kernel
+# install initramfs
 # genkernel --kernel-config=/usr/src/linux-tcel --install initramfs
